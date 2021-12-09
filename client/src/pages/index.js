5d228439beae5c8b850a23990d613a4d39e40e2a
Angham116 <angham.1161995@hotmail.com>
2021-12-09 09:54:04 +0200

    handle react routes

---
+++ client/src/pages/index.js
@@ -0,0 +1,11 @@
+import Signup from './signup';
+import Login from './login';
+import Projects from './projects';
+import NotFound from './notFound';
+
+export {
+  Signup,
+  Login,
+  Projects,
+  NotFound,
+}
---
+++ client/src/pages/notFound/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <div>
+      <h2>Page Not Found | Error 404</h2>
+    </div>
+  )
+}
